3c2bdd7fc2e8d25e0ef862ec329dc5680575b456
Cristiano A <cristiano.mamaral@telefonica.com>
2023-07-14 13:59:59 -0300

    first commit

---
+++ quiz2.rs
@@ -0,0 +1,83 @@
+// quiz2.rs
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+
+// Let's build a little machine in the form of a function.
+// As input, we're going to give a list of strings and commands. These commands
+// determine what action is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+// The exact form of this will be:
+// - The input is going to be a Vector of a 2-length tuple, [(0, 1), (0, 1)]
+//   the first element is the string, the second one is the command.  [("TXT", command), ("TXT", command)]
+// - The output element is going to be a Vector of strings.
+// No hints this time!
+
+pub enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+
+pub mod my_module {
+    use super::Command;
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+
+        for (string, command) in input.iter() {
+            let final_str = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(size) => {
+                    let mut s = string.to_owned();
+
+                    for i in 0..*size {
+                        s.push_str("bar");
+                    }
+
+                    s.to_string()
+                }
+            };
+            output.push(final_str);
+        }
+        println!("{:?}", output);
+        output
+    }
+}
+
+fn main() {
+    use crate::my_module::transformer;
+    let output = transformer(vec![
+        ("hello".into(), Command::Uppercase),
+        (" all roads lead to rome! ".into(), Command::Trim),
+        ("bar".into(), Command::Append(5)),
+        ("foo".into(), Command::Append(1)),
+    ]);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::Command;
+    use crate::my_module::transformer;
+
+    #[test]
+    fn it_works() {
+        let output = transformer(vec![
+            ("hello".into(), Command::Uppercase),
+            (" all roads lead to rome! ".into(), Command::Trim),
+            ("foo".into(), Command::Append(1)),
+            ("bar".into(), Command::Append(5)),
+        ]);
+        assert_eq!(output[0], "HELLO");
+        assert_eq!(output[1], "all roads lead to rome!");
+        assert_eq!(output[2], "foobar");
+        assert_eq!(output[3], "barbarbarbarbarbar");
+    }
+}
